4b8cc96f5839d16231f20e10647b31a798f5dbf6
Daniel Schwabacher <danielschwabacher@gmail.com>
2025-01-12 13:21:47 -0500

    update package.json to use vite + standardized cypress commands

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,11 +3,10 @@
 import "@mantine/core/styles.css";
 
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
 import { useOptionalUser } from "~/utils";
 import { MarketingHomePage } from "~/components/MarketingHomePage";
 
-export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
+export const meta: MetaFunction = () => [{ title: "Groove Stack" }];
 
 export default function Index() {
   const user = useOptionalUser();
